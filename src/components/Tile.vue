3064fd5f5269c39535c8a0b4a3acb2462016acee
Carmen V Ramos <carmenvramos@yahoo.com>
2018-05-31 16:07:22 -0700

    pokemon list showing to results

---
+++ src/components/Tile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="tiles">
+        <h1 v-for="pokemon in list"
+        v-bind:key="pokemon.pokemon">
+        {{pokemon.pokemon}}
+        </h1>
+    </div> 
+</template>
+
+<script>
+    import pokemon from '../pokemon.js';
+
+    export default {
+        data() {
+            return {
+                list: pokemon
+            }
+        }
+    }
+</script>
