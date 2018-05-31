762ec705fb9c6ecb80f9bbe0ee2637d15ac7126d
Chris Golden <chrgolden@gmail.com>
2018-05-31 15:14:00 -0700

    results area showing

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,20 @@
     <section class="header">
       <HeaderArea/>
     </section>
+    <section class="results">
+      <ResultsArea/>
+    </section>
   </div>
 </template>
 
 <script>
 import HeaderArea from './components/Header.vue';
+import ResultsArea from './components/Results.vue';
 
 export default {
       components: {
-      HeaderArea
+      HeaderArea,
+      ResultsArea
     }
 }
 
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>Pokedex</h1>
+    <h1>Results</h1>
   </div>
 </template>
 
 <script>
   export default {
 
-  };
+  }
 </script>
