cd003ab299e3ab2b86c9e14fef982e9ecf112841
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-05-31 15:43:15 -0700

    Add initial basic styling

--- src/App.vue
+++ src/App.vue
@@ -31,5 +31,32 @@ export default {
 </script>
 
 <style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: #eee;
+}
 
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 90%;
+  margin: auto;
+}
+
+.header {
+  background: lightsteelblue;
+  width: 90%;
+  flex-grow: 2;
+}
+
+.results {
+  background: lightsalmon;
+  width: 90%;
+  flex-grow: 5;
+}
 </style>
