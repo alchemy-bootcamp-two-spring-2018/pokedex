12ffb4e5f1f0b6fd0b561b808c96944badbf24b7
Easton Gorishek <ieaston26@gmail.com>
2018-05-31 14:42:54 -0700

    linked up header

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>This is a Header</h1>
+    <section class="filter">
+      <input type="checkbox" value="type">Type</input>
+      <input type="checkbox" value="HP">HP</input>
+    </section>
+
+    <section class="sort">
+      <select>
+        <option value="Name">Name</option>
+        <option value="Type">Type</option>
+      </select>
+    </section>
+    
+  </div>
+</template>
+
+<script>
+
+import Filter from './Filter.vue'
+import Sort from './Sort.vue'
+
+export default {
+
+  components: {
+    Filter,
+    Sort
+  }
+
+};
+
+</script>
+
+<style>
+
+</style>
