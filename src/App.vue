924c96af52da1e5e73fde98ef26ab8fe98181775
Chris Golden <chrgolden@gmail.com>
2018-05-31 15:10:10 -0700

    got header to show

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
+    App area
+    <section class="header">
+      <HeaderArea/>
+    </section>
   </div>
 </template>
 
 <script>
+import HeaderArea from './components/Header.vue';
 
 export default {
-
+      components: {
+      HeaderArea
+    }
 }
 
 </script>
