d390b9a92ca627e65ff31124b1d8c0cddd3792bf
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-01 15:27:47 -0700

    sort by connected to page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,17 @@
   <div>
     <h1>Pokedex</h1>
     <filterArea :filter="filter"/>
-
+    <sortArea/>
   </div>
 </template>
 
 <script>
   import filterArea from "./Filter.vue";
+  import sortArea from "./Sort.vue";
   export default {
     components: {
-      filterArea
+      filterArea,
+      sortArea
     },
     props: ['filter']
   };
---
+++ src/components/Sort.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>Sort By</h3>
+        <select>
+            <option>None</option>
+            <option value="pokemon">Name(A-Z)</option>
+            <option value="defense">Defense(High-Low)</option>
+            <option value="attack">Attack(High-Low)</option>
+        </select>  
+    </div>
+
+</template>
+<script>
+    export default {
+
+    }
+</script>
