65614c98988c7691a510138ded03e21bc596fa08
Mark Myers <markalope@gmail.com>
2018-05-31 23:46:09 -0700

    got conditional colors working in tile.vue

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <ul>
-      <li> {{ pokemon.pokemon }} </li>
-      <li> HP: {{pokemon.hp}} </li>
-      <li> Type: {{pokemon.type_1}} </li>
-      <li> <img :src="pokemon.url_image"></li>
-    </ul>
+  <div class = "container" >
+    <div class = "tile" v-bind:style="{ background: pokemon.color_1 }">
+      <img :src="pokemon.url_image">
+      <p class = "name"> {{ pokemon.pokemon }} </p>
+      <p> HP: {{pokemon.hp}} </p>
+      <p> Type: {{pokemon.type_1}} </p>
+      
+    </div>
   </div>
 </template>
 
@@ -20,11 +21,38 @@ export default {
 </script>
 
 <style>
-ul {
-list-style: none;
+
+p {
+color: white;
+font-size: 16px;
+font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+text-transform: capitalize;
 
 }
+.name {
+  text-transform: uppercase;
+  font-size: 18px;
+}
+
 img {
-  width: 100px;
+  width: 150px;
+  background-color: white;
+  border: 1px solid black;
+
+}
+.tile {
+  text-align: center;
+  display: inline-block;
+  weight: 300 px;
+  border: 10px solid yellow;
+  border-radius: 15px;
+  margin: 10px;
+  padding: 10px;
+  
+}
+.container {
+min-width: 250px;
+display: inline;
+
 }
 </style>
