a52bb18ec62fd1c2869541fa8923618bd94d5f05
rbtprograms <rbt61690@gmail.com>
2018-06-01 22:47:31 -0700

    some more styling

--- src/components/tile.vue
+++ src/components/tile.vue
@@ -5,9 +5,21 @@
     v-bind:key = 'pokeDex.id'
     v-bind:style="{ backgroundColor: pokeDex.color_1 }" 
     class="containerTile"> 
-      <span>{{pokeDex.pokemon}}</span>
-      <span>{{pokeDex.type_1}}</span>
+      <span
+      v-bind:style="{ backgroundColor: pokeDex.color_f}"
+      >{{pokeDex.pokemon}}</span>
+      <span
+      v-bind:style="{ backgroundColor: pokeDex.color_f}"
+      >{{pokeDex.type_1}}</span>
       <img v-bind:src="pokeDex.url_image">
+      <div>
+        <span>HP: {{pokeDex.hp}}</span>
+        <span>Attack: {{pokeDex.attack}}</span>
+        <span>Defense: {{pokeDex.defense}}</span>
+        <span>Sp.Attack: {{pokeDex.special_attack}}</span>
+        <span>Sp.Defense: {{pokeDex.special_defense}}</span>
+        <span>Speed: {{pokeDex.speed}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -42,16 +54,19 @@ a {
   display: inline-block;
   background-color: rgb(245, 245, 245);
   margin: 5px;
+  border-radius: 7.5px;
+  width: 300px;
+  opacity: 0.75;
 }
 
 .containerTile:hover{
-  background-color: rgb(211, 211, 211);
+  opacity: 1;
 }
 span {
   display: block;
 }
 
 img {
-  width: 75px;
+  width: 200px;
 }
 </style>
