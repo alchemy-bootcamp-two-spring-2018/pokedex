2cebccacb9bb92ea901b7d27b636fedaaf3820dc
rbtprograms <rbt61690@gmail.com>
2018-06-04 14:27:40 -0700

    eslint redid

--- src/dropdownData.js
+++ src/dropdownData.js
@@ -1,59 +1,59 @@
 export default [
-    {
-        'name': ''
-    },
-    {
-        'name': 'Fire'
-    },
-    {
-        'name': 'Water'
-    },
-    {
-        'name': 'Grass'
-    },
-    {
-        'name': 'Electric'
-    },
-    {
-        'name': 'Psychic'
-    },
-    {
-        'name': 'Ice'
-    },
-    {
-        'name': 'Dragon'
-    },
-    {
-        'name': 'Dark'
-    },
-    {
-        'name': 'Fairy'
-    },
-    {
-        'name': 'Normal'
-    },
-    {
-        'name': 'Fighting'
-    },
-    {
-        'name': 'Flying'
-    },
-    {
-        'name': 'Poison'
-    },
-    {
-        'name': 'Ground'
-    },
-    {
-        'name': 'Rock'
-    },
-    {
-        'name': 'Bug'
-    },
-    {
-        'name': 'Ghost'
-    },
-    {
-        'name': 'Steel'
-    },
-]
+  {
+    'name': ''
+  },
+  {
+    'name': 'Fire'
+  },
+  {
+    'name': 'Water'
+  },
+  {
+    'name': 'Grass'
+  },
+  {
+    'name': 'Electric'
+  },
+  {
+    'name': 'Psychic'
+  },
+  {
+    'name': 'Ice'
+  },
+  {
+    'name': 'Dragon'
+  },
+  {
+    'name': 'Dark'
+  },
+  {
+    'name': 'Fairy'
+  },
+  {
+    'name': 'Normal'
+  },
+  {
+    'name': 'Fighting'
+  },
+  {
+    'name': 'Flying'
+  },
+  {
+    'name': 'Poison'
+  },
+  {
+    'name': 'Ground'
+  },
+  {
+    'name': 'Rock'
+  },
+  {
+    'name': 'Bug'
+  },
+  {
+    'name': 'Ghost'
+  },
+  {
+    'name': 'Steel'
+  },
+];
--- src/sortDropdown.js
+++ src/sortDropdown.js
@@ -1,22 +1,26 @@
 export default [
-    {
-        'name': 'Name',
-        'value': 'pokemon'
-    },
-    {
-        'name': 'Number',
-        'value': 'id'
-    },
-    {
-        'name': 'Attack',
-        'value': 'attack'
-    },
-    {
-        'name': 'Defence',
-        'value': 'defense'
-    },
-    {
-        'name': 'Hit Points',
-        'value': 'hp'
-    },
-]
+  {
+    'name': 'Name',
+    'value': 'pokemon'
+  },
+  {
+    'name': 'Number',
+    'value': 'id'
+  },
+  {
+    'name': 'Attack',
+    'value': 'attack'
+  },
+  {
+    'name': 'Defence',
+    'value': 'defense'
+  },
+  {
+    'name': 'Hit Points',
+    'value': 'hp'
+  },
+  {
+    'name': 'Speed',
+    'value': 'speed'
+  },
+];
